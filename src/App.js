fd89059d63ad790c635b998f5132e6e60b365f0c
nandini-naik09 <nandinipardeshi99@gmail.com>
2023-05-14 22:47:02 +0530

    Components Created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Header from './Header'
+import Footer from './Footer'
+import Component1 from './Component1'
+import Component2 from './Component2'
+import Component3 from './Component3'
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         Welcome to React
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header/>
+      <h1>App</h1>
+      <Component1/>
+      <Component2/>
+      <Component3/>
+      <Footer/>
     </div>
-  );
+  )
 }
-
-export default App;
